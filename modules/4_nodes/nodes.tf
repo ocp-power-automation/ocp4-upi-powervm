0e5fbd0060897d637391637b7499029aeccd5916
Yussuf Shaikh <yussuf@us.ibm.com>
2020-09-03 18:01:37 +0000

    Support for OCP 4.6 installation
    Fixes #104
    
    Signed-off-by: Yussuf Shaikh <yussuf@us.ibm.com>

--- modules/4_nodes/nodes.tf
+++ modules/4_nodes/nodes.tf
@@ -23,18 +23,50 @@ resource "random_id" "label" {
     byte_length = "2"
 }
 
+# FIXME:
+# This will restart NetworkManager on all RHCOS nodes every 5 mins.
+# Required because RHCOS nodes fail to get the network address and wait continously for the ignition URL.
+# Remove when a fix is available or if not required.
+data "ignition_systemd_unit" "restart-NetworkManager" {
+    name        = "NetworkManager.service"
+    dropin {
+        name    = "restart.conf"
+        content = <<EOF
+[Service]
+Restart=always
+RuntimeMaxSec=300
+EOF
+    }
+}
+
+data "ignition_file" "dhcp-timeout" {
+    overwrite   = true
+    mode        = "755" // 0644
+    path        = "/etc/NetworkManager/conf.d/99-dhcp-timeout.conf"
+    content {
+        content = <<EOF
+[connection]
+ipv4.dhcp-timeout=2147483647
+EOF
+    }
+}
+
 #bootstrap
 data "ignition_config" "bootstrap" {
-    append {
+    merge {
         source  = "http://${var.bastion_ip}:8080/ignition/bootstrap.ign"
     }
     files       = [
         data.ignition_file.b_hostname.rendered,
+        data.ignition_file.dhcp-timeout.rendered
+    ]
+    systemd = [
+        data.ignition_systemd_unit.restart-NetworkManager.rendered
     ]
 }
 
 data "ignition_file" "b_hostname" {
-    filesystem  = "root"
+    overwrite   = true
     mode        = "420" // 0644
     path        = "/etc/hostname"
     content {
@@ -86,18 +118,21 @@ data "ignition_systemd_unit" "ramdisk" {
 
 data "ignition_config" "master" {
     count       = var.master["count"]
-    append {
+    merge {
         source  = "http://${var.bastion_ip}:8080/ignition/master.ign"
     }
     files       = [
         element(data.ignition_file.m_hostname.*.rendered, count.index),
+        data.ignition_file.dhcp-timeout.rendered
     ]
-    systemd     = var.mount_etcd_ramdisk ? data.ignition_systemd_unit.ramdisk.*.rendered : null
+    systemd     = concat(
+                    [data.ignition_systemd_unit.restart-NetworkManager.rendered],
+                    data.ignition_systemd_unit.ramdisk.*.rendered)
 }
 
 data "ignition_file" "m_hostname" {
     count       = var.master["count"]
-    filesystem  = "root"
+    overwrite   = true
     mode        = "420" // 0644
     path        = "/etc/hostname"
     content {
@@ -145,7 +180,7 @@ resource "openstack_compute_instance_v2" "master" {
 #worker
 data "ignition_file" "w_hostname" {
     count       = var.worker["count"]
-    filesystem  = "root"
+    overwrite   = true
     mode        = "420" // 0644
     path        = "/etc/hostname"
 
@@ -158,11 +193,15 @@ EOF
 
 data "ignition_config" "worker" {
     count       = var.worker["count"]
-    append {
+    merge {
         source  = "http://${var.bastion_ip}:8080/ignition/worker.ign"
     }
     files       = [
         element(data.ignition_file.w_hostname.*.rendered, count.index),
+        data.ignition_file.dhcp-timeout.rendered
+    ]
+    systemd = [
+        data.ignition_systemd_unit.restart-NetworkManager.rendered
     ]
 }
 
