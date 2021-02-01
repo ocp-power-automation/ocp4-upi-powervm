885cddfb6f9af7fcb9df2cc7394146a45285f8d9
Yussuf Shaikh <yussuf.shaikh@ibm.com>
2021-02-01 16:54:34 +0000

    Use hostnames instead of IP in inventory
    Fixes #163
    
    Signed-off-by: Yussuf Shaikh <yussuf.shaikh@ibm.com>

--- modules/5_install/install.tf
+++ modules/5_install/install.tf
@@ -67,10 +67,10 @@ locals {
     }
 
     inventory = {
-        bastion_ip      = var.bastion_ip
-        bootstrap_ip    = var.bootstrap_ip
-        master_ips      = var.master_ips
-        worker_ips      = var.worker_ips
+        bastion_host    = "${var.cluster_id}-bastion"
+        bootstrap_host  = "bootstrap"
+        master_hosts    = [for ix in range(length(var.master_ips)) : "master-${ix}"]
+        worker_hosts    = [for ix in range(length(var.worker_ips)) : "worker-${ix}"]
     }
 
     proxy = {
